7a3c85ebe3e86190a0966e1e7329d4e2b52a226f
Alejandro Zapiola <lodeale@gmail.com>
2016-07-12 14:22:30 -0300

    se arreglan comentarios

--- app.py
+++ app.py
@@ -28,43 +28,13 @@ def main (argv):
 			fileName2 = arg
 		elif opt in ("-s", "--sheetName"):
 			sheetName = arg
-	# print 'File Name 1 => ', fileName1
-	# print 'File Name 2 => ', fileName2
-	# print 'Sheet Name => ', sheetName
 	
-
 	res = MyCsv(fileName1)
 	xls = MyXls(fileName2,sheetName)
-	print "===================================="
-	print "CSV"
-	print "===================================="
+
 	print res.results()
-	print "===================================="
-	print "XLSX"
-	print "===================================="
 	print xls.results()
 
 
 if __name__ == "__main__":
    main(sys.argv[1:])
-
-
-# if len(sys.argv) >= 2:
-# 	print 'File Name 1 => ', fileName1
-# 	print 'File Name 2 => ', fileName2
-# 	print 'Sheet Name => ', sheetName 
-
-# 	# fileName = sys.argv[1]
-# 	# fileNameXls = sys.argv[2]
-# 	# sheetName = sys.argv[3]
-
-# 	res = MyCsv(fileName1)
-# 	#makeSQL = GenerateSql()
-# 	xls = MyXls(fileName2,sheetName)
-
-# 	res.get(fileName1)
-# 	xls.show()
-# 	#makeSQL.make(res.bodyCsv)
-# else:
-# 	print "Error: Debe pasar el nombre del archivo"
-
